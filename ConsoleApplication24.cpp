9cd0f4a5b7acee2fdeff7b984ce2d2c11b1bee63
Ari Badovska <160342977+AriSnowie97@users.noreply.github.com>
2024-11-16 12:30:34 +0200

    Add files via upload

---
+++ ConsoleApplication24.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string>
+#include <regex>
+using namespace std;
+int main(){
+	string data = "16-11-2024";
+	regex Regexdata("^\\d{2}-\\d{2}-\\d{4}$");
+	if (regex_match(data, Regexdata)) {
+		cout << "Date is correct" << endl;
+	}
+	else {
+		cout << "Date is incorrect" << endl;
+	}
+	return 0;
+}
